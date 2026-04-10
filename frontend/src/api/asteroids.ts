3a7199f0004f2a1386cc11549d7a53c6656e4c90
Andrii <zh.stebenev191104@gmail.com>
2026-04-10 13:15:08 +0100

    URL change

--- frontend/src/api/asteroids.ts
+++ frontend/src/api/asteroids.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import type { NeoFeedResponse, Asteroid } from "../types/asteroid"
 
-const BASE_URL = "http://localhost:3000/api/asteroids"
+const BASE_URL = "https://asteroids-explorer-backend.onrender.com/api/asteroids"
 
 export const getFeed = async (start_date: string, end_date: string): Promise<NeoFeedResponse> => {
     const response = await axios.get(`${BASE_URL}/feed`, {
